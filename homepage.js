2cc8bcfae8355e0e350e9b1f9ef018363f14cd50
Maria Montagel <maria.montagel@adesso.de>
2023-02-10 09:03:17 +0100

    fertig

--- homepage.js
+++ homepage.js
@@ -1,62 +1,159 @@
 
 const input = document.getElementById('formFile')
-let datenarray;
+let datenarray = [];
+let powerArray = [];
+let tag = "2:50 - 23:55";   
+let sum = 0;
+    let startTag = 1;
+
+
+//Sat Dec 30 1899 10:10:00
 
 input.addEventListener('change', () => {
   readXlsxFile(input.files[0]).then((data) => {
     datenarray = data;
+    localStorage.setItem("datenPV", datenarray);
     //console.log(data);
     // `data` is an array of rows
     // each row being an array of cells.
     console.log(data[0]); // ganze Zeile
-    console.log(data[0][1]); // Uhrzeit
-    let sum = 0;
-    // die 5 Watt (pro min)  
-    tag = document.querySelector('#tag').value.trim();
+    console.log(data[800][1]); // Uhrzeit
+    let t = 1;
+    tag = document.querySelector('#tag').value.trim(); // 1
+    let gesSum = 0;
 
-    for (let i = 0; i < data.length; i++) {
-      if (data[i][0] == tag) {
-        console.log(data[i][0]);
-        sum = sum + (data[i][2] / 12)/1000;
-      }
+    // if (data[i][0] == `Tag ${startTag}` && data[t][1] == "Sat Dec 30 1899 10:10:00 GMT+0100 (Mitteleuropäische Normalzeit)") {
+
+
+    for (let i = 1; i < datenarray.length; i++) {
+      // Wenn Zeit und Tag stimmen
+      if (datenarray[i][0] == `Tag ${tag}`) {
+        sum = sum + (datenarray[i][2] / 12)/1000;
+      } 
     }
 
     document.getElementById('#tag1').innerHTML = tag;
-    document.getElementById('#kwh').innerHTML = sum.toFixed(3) + " kwh";
-    document.getElementById('#time').innerHTML = "2:30 - 23:55";
+    document.getElementById('#kwh').innerHTML = sum.toFixed(3) + " kWh";
+    document.getElementById('#time').innerHTML = "2:30";
     console.log(sum);
+    console.log(gesSum);
+
   })
   
 })
 
+function berechneBisTag (tag) {
+  let summe;
+  for (let i = 1; i < datenarray.length; i++) {
+    // Wenn Zeit und Tag stimmen
+    if (datenarray[i][0] == `Tag ${startTag}`) {
+      summe = summe + (datenarray[i][2] / 12)/1000;
+    } else if (startTag < tag) {
+      startTag++;        
+    }
+  }
+  return summe
+}
 
-const dailyChargeNeeded = [];
-let currentCharge = 0;
+/*
+ for (let i = 1; i < data.length; i++) {
+    // Wenn Zeit und Tag stimmen
+    if (data[i][0] == `Tag ${startTag}` && data[t][1] == "08:25") {
+      console.log(data[i][0]);
+      sum = sum + (data[i][2] / 12)/1000;
+    } else if (startTag < tag && data[t][1] == "08:25" ) {
+      startTag++;        
+    } else {
+      t++;
+    }
+  }*/
+  function tagPlanen () {    
+  let planTag = document.querySelector('#PlanTag').value.trim(); // 1  
+  console.log(planTag);
+
+  document.getElementById('#test').innerHTML = "sdfghj";
+  document.getElementById('#speicherstrom').innerHTML = "sdfghj";
+
+ // let speicherStrom = berechneBisTag(planTag);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const dailyChargeNeeded = []; // 25 kwh
+let currentCharge = 0; // Strom im Speicher
+const chargingTimes = [];
 
 function addChargeNeed(charge) {
   dailyChargeNeeded.push(charge);
 }
 
-function checkBattery(currentCharge) {
-  for (let i = 0; i < dailyChargeNeeded.length; i++) {
-    currentCharge -= dailyChargeNeeded[i];
-    if (currentCharge < 0) {
-      console.log(`Du musst dein Auto mit ${Math.abs(currentCharge)}kWh extra beladen, um deine nächste Fahrt zu beenden`);
-      return false;
-    }
-  }
-  console.log(`Du bist bereit für deine Fahrt! Keine extra Ladung wird vor deiner nächsten Fahrt benötigt`)
-  return true;
+function addChargingTime(startTime, endTime) {
+  chargingTimes.push({ startTime, endTime });
 }
 
+function checkBattery(currentCharge) {
+  for (let i = 0; i < dailyChargeNeeded.length; i++) {
+    currentCharge -= dailyChargeNeeded[i]; 
+    if (currentCharge < 0) { // zu wenig aus Speicher
+      const chargeNeeded = Math.abs(currentCharge); 
+      let availableChargingTime = 0;
+      for (let j = 0; j < chargingTimes.length; j++) {
+        availableChargingTime += (chargingTimes[j].endTime - chargingTimes[j].startTime);
+      }
+      const chargingTime = chargeNeeded / 4;
+      if (chargingTime > availableChargingTime) {
+        console.log(`You need to charge your car with ${chargeNeeded}kWh to complete your trip, but you only have ${availableChargingTime} hours available to charge your car.`);
+        return false;
+      } else {console.log() 
+      }
+    }
+  }
+}
+
+function plane() {
+
+}
 
 
 function getLivePosition() {
     console.log("Verbindung mit Batteriespeicher");
     tag = document.querySelector('#tag').value.trim();
     console.log(tag);
-
 }
 
-let tag
 
---
+++ tagesplan.js
@@ -0,0 +1,161 @@
+
+const input = document.getElementById('formFile')
+let datenarray = [];
+let powerArray = [];
+let tag = "2:50 - 23:55";   
+let sum = 0;
+    let startTag = 1;
+
+
+//Sat Dec 30 1899 10:10:00
+
+input.addEventListener('change', () => {
+  readXlsxFile(input.files[0]).then((data) => {
+    datenarray = data;
+    localStorage.setItem("datenPV", datenarray);5
+
+  })
+  
+})
+
+function berechneBisTag (tag) {
+  let summe = 0;
+  for (let i = 1; i < datenarray.length; i++) {
+    // Wenn Zeit und Tag stimmen
+    if (datenarray[i][0] == `Tag ${startTag}`) {
+      summe = summe + (datenarray[i][2] / 12)/1000;
+    } else if (startTag < tag) {
+      startTag++;        
+    }
+  }
+  console.log(summe);
+  return summe.toFixed(3);
+}
+
+/*
+ for (let i = 1; i < data.length; i++) {
+    // Wenn Zeit und Tag stimmen
+    if (data[i][0] == `Tag ${startTag}` && data[t][1] == "08:25") {
+      console.log(data[i][0]);
+      sum = sum + (data[i][2] / 12)/1000;
+    } else if (startTag < tag && data[t][1] == "08:25" ) {
+      startTag++;        
+    } else {
+      t++;
+    }
+  }*/
+  function tagPlanen () {    
+    // Werte aufnehmen
+  let planTag = document.querySelector('#PlanTag').value.trim(); // 1  
+  let powerNeeded = document.querySelector('#powerneeded').value.trim();  
+  let ladeleistung = document.querySelector('#ladeleistung').value.trim(); 
+  let startTime = document.querySelector('#startZeit').value.trim(); 
+  let endTime = document.querySelector('#endZeit').value.trim(); 
+
+
+
+  // Aufladedauer
+  let ladedauer = powerNeeded/ladeleistung;
+
+  // verfügebarer Strom
+  let speicherStrom = berechneBisTag(planTag);
+  let speicherStromÜbrig = 0;
+
+ 
+ // Aufladen Speicher / frembezogen
+  let ladenSp = ladedauer;
+  let ladeF = 0;
+
+
+  // fremdbezogener Strom
+  let fStrom = speicherStrom-powerNeeded;
+  console.log(fStrom);
+  if ( fStrom < 0) {
+    fStrom = Math.abs(fStrom);
+    ladenSp =  speicherStrom/ladeleistung;
+    ladeF = fStrom/ladeleistung;
+  } else {
+    speicherStromÜbrig = fStrom;    
+    fStrom = 0;
+  }
+
+  // Zur Uhrzeit
+  // Dauer vergleichen
+  let differenzDate = calculateData(startTime, endTime);
+  console.log(hours1);
+  console.log(min1);
+  let a;
+
+  let timediff = `${hours1}`; 
+  timediff = parseFloat(timediff) + min1;
+  console.log(timediff);
+
+  
+  if (timediff < ladedauer) {
+    let diff = timediff - ladedauer;
+    console.log("NOOO  " + diff);
+     // Error anzeigen
+     a =  document.getElementById('#error');
+     a.innerHTML = `Sie brauchen mehr Zeit zum laden! Es fehlen ${Math.abs(diff).toFixed(1)} h`;
+     a.style.visibility = 'visible';    
+
+  } else {    
+    console.log("klppt")
+    a =  document.getElementById('#error');
+
+     a.style.visibility = 'hidden';
+
+  }
+
+
+
+  // Werte ergänzen
+  document.getElementById('#speicherstrom').innerHTML = speicherStrom + " kWh";
+  document.getElementById('#ladedauer').innerHTML = ladedauer.toFixed(1)+ " h";
+  document.getElementById('#fstrom').innerHTML = fStrom.toFixed(3) + " kWh";
+  document.getElementById('#nachAufladen').innerHTML = speicherStromÜbrig.toFixed(3) + " kWh";
+  document.getElementById('#ladedauerB').innerHTML = ladenSp.toFixed(1) + " h";
+  document.getElementById('#ladedauerF').innerHTML = ladeF.toFixed(1) + " h";
+  document.getElementById('#zeitV').innerHTML = differenzDate + " (hours:min), (" + timediff.toFixed(1) + " h)";
+}
+
+const calculateData = (startTimeStamp, finishTimeStamp) => {    
+    //23:23
+
+    let h1 = startTimeStamp.slice(0,2);
+    var m1 = startTimeStamp.slice(3,6);    
+    console.log(h1, m1);
+
+    var h2 = finishTimeStamp.slice(0,2);
+    var m2 = finishTimeStamp.slice(3,6);
+
+    // Monat und Tag sind vertauscht ->Monat vorne
+    var date1 = new Date(2022, 01, 01, h1, m1, 0);
+
+  var date2   = new Date(2022, 01, 01, h2, m2, 0);
+
+    var diff = date2 - date1;
+    console.log(diff); // milliseconds 
+
+    var msec = diff;
+    var hours = Math.floor(msec / 1000 / 60 / 60);
+    msec -= hours * 1000 * 60 * 60;
+    var minutes = Math.floor(msec / 1000 / 60);
+    msec -= minutes * 1000 * 60;
+   
+    // diff = 28800000 => hh = 8, mm = 0, ss = 0, msec = 0
+    console.log(hours);
+    console.log(minutes);
+    // Konvertieren zu HOURS:MIN:MS
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    min1 = 1 / (60/minutes);
+    hours1= hours;
+
+    return hours + ":" + minutes;
+}
+let hours1;
+
+let min1;
+
+
